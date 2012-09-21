961712f27e3ee147deadb9e3230aaee2bab761c7
Tim Dettrick <tim@dettrick.net>
2012-09-21 14:57:32 +1000

    Adding some examples for those rusty with Nose.

--- test/test_example.py
+++ test/test_example.py
@@ -1,4 +1,13 @@
+from nose.tools import eq_, ok_, raises
+
 class TestExample:
-    
-    def test(self):
-        pass
+
+    def test_ok(self):
+        ok_(True)
+
+    def test_eq(self):
+        eq_(2 + 2, 4)
+
+    @raises(TypeError, ValueError)
+    def test_raises(self):
+        raise TypeError("This is expected.")
